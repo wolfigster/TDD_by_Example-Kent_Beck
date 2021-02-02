5c4e9b127d8e5900252bf50b613aed266b36fe07
Johannes | A <Jeis2519@gmail.com>
2021-02-02 11:54:01 +0100

    remove setUp method from TestCaseTest -> no longer needed

--- test.py
+++ test.py
@@ -26,13 +26,10 @@ class WasRun(TestCase):
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
-        self.test.run()
-        assert ("setUp testMethod " == self.test.log)
+        test = WasRun("testMethod")
+        test.run()
+        assert ("setUp testMethod " == test.log)
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
