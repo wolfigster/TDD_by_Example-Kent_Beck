add4b15754643f7bfeb58282781787fa71f5c8cc
Johannes | A <Jeis2519@gmail.com>
2021-02-02 12:07:17 +0100

    make TestResult use a constant for reporting

--- test.py
+++ test.py
@@ -17,8 +17,11 @@ class TestCase:
 
 
 class TestResult:
+    def __init__(self):
+        self.runCount = 1
+
     def summary(self):
-        return "1 run, 0 failed"
+        return "%d run, 0 failed" % self.runCount
 
 
 class WasRun(TestCase):
