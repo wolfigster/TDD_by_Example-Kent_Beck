0ddb92d32e448866d87527d28d8ea3d1aa56f1d5
Johannes | A <Jeis2519@gmail.com>
2021-02-02 11:49:54 +0100

    remove wasSetUp flag and add log in WasRun.testMethod -> breaking previous test

--- test.py
+++ test.py
@@ -18,10 +18,10 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
 
     def setUp(self):
         self.wasRun = None
-        self.wasSetUp = 1
         self.log = "setUp "
 
 
