6b92a72f2f261a222b67ca00391bee86e828aba0
Johannes | A <Jeis2519@gmail.com>
2021-02-02 12:26:42 +0100

    report failures in summary

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ class TestResult:
         self.errorCount = self.errorCount + 1
 
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 
 class WasRun(TestCase):
