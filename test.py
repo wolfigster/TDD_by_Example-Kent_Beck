67a896837406e60230c34d2ab71eea244362820a
Johannes | A <Jeis2519@gmail.com>
2021-02-02 11:51:38 +0100

    change the expected value to test both

--- test.py
+++ test.py
@@ -35,7 +35,7 @@ class TestCaseTest(TestCase):
 
     def testSetUp(self):
         self.test.run()
-        assert ("setUp " == self.test.log)
+        assert ("setUp testMethod " == self.test.log)
 
 
 TestCaseTest("testRunning").run()
