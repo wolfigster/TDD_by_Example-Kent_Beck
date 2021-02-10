7e65fa63fb5f19501e980c385a86914937c010e8
Johannes W <Jeis2519@gmail.com>
2021-02-10 11:21:13 +0100

    extend testMultiplication

--- PART-I/src/test/java/tdd/money/Test.java
+++ PART-I/src/test/java/tdd/money/Test.java
@@ -8,5 +8,7 @@ public class Test extends TestCase {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
 }
