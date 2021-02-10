a98b0ea3e053d947df3e491730473af5bceeef3c
Johannes W <Jeis2519@gmail.com>
2021-02-10 12:34:33 +0100

    add test to check currency

--- PART-I/src/test/java/tdd/money/MoneyTest.java
+++ PART-I/src/test/java/tdd/money/MoneyTest.java
@@ -13,4 +13,9 @@ public class MoneyTest extends TestCase {
 
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
+
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
 }
