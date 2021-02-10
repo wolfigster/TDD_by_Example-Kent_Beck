3fd166c137d2ef1773fada70e48fd2e288cff081
Johannes W <Jeis2519@gmail.com>
2021-02-10 12:15:39 +0100

    compare Francs with Dollars

--- PART-I/src/test/java/tdd/money/MoneyTest.java
+++ PART-I/src/test/java/tdd/money/MoneyTest.java
@@ -7,7 +7,10 @@ public class MoneyTest extends TestCase {
     public void testMoneyEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 }
