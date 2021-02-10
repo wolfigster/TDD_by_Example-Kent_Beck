a33e42cd38f1421a14cdd50fa59c7b7e07ab22ab
Johannes W <Jeis2519@gmail.com>
2021-02-10 12:28:28 +0100

    use the factory method also for the Franc tests

--- PART-I/src/test/java/tdd/money/FrancTest.java
+++ PART-I/src/test/java/tdd/money/FrancTest.java
@@ -5,8 +5,8 @@ import junit.framework.TestCase;
 public class FrancTest extends TestCase {
 
     public void testFrancMultiplication() {
-        Franc five = new Franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 }
