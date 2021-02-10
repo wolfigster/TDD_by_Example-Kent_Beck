d54dbf87186ebe5551986c12b9a36c96fdc79f49
Johannes W <Jeis2519@gmail.com>
2021-02-10 12:22:43 +0100

    introduce factory method that returns Dollar

--- PART-I/src/main/java/tdd/money/Money.java
+++ PART-I/src/main/java/tdd/money/Money.java
@@ -3,6 +3,10 @@ package tdd.money;
 public class Money {
     protected int amount;
 
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
--- PART-I/src/test/java/tdd/money/DollarTest.java
+++ PART-I/src/test/java/tdd/money/DollarTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 public class DollarTest extends TestCase {
 
     public void testDollarMultiplication() {
-        Dollar five = new Dollar(5);
+        Dollar five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
