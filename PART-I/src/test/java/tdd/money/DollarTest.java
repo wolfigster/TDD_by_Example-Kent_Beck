715f4193acc903fc4e83a31f973817a0c44df7e1
Johannes W <Jeis2519@gmail.com>
2021-02-10 12:27:07 +0100

    use factory method in tests

--- PART-I/src/test/java/tdd/money/DollarTest.java
+++ PART-I/src/test/java/tdd/money/DollarTest.java
@@ -6,7 +6,7 @@ public class DollarTest extends TestCase {
 
     public void testDollarMultiplication() {
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 }
--- PART-I/src/test/java/tdd/money/MoneyTest.java
+++ PART-I/src/test/java/tdd/money/MoneyTest.java
@@ -5,12 +5,12 @@ import junit.framework.TestCase;
 public class MoneyTest extends TestCase {
 
     public void testMoneyEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
 
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        assertFalse(new Franc(5).equals(Money.dollar(5)));
     }
 }
