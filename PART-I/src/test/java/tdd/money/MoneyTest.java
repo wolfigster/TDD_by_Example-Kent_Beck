4733cd77cc30d4c003f8249a2aadda1db0813119
Johannes W <Jeis2519@gmail.com>
2021-02-10 14:28:50 +0100

    remove unnecessary test testDifferentClassEquality

--- PART-I/src/test/java/tdd/money/MoneyTest.java
+++ PART-I/src/test/java/tdd/money/MoneyTest.java
@@ -8,18 +8,11 @@ public class MoneyTest extends TestCase {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
