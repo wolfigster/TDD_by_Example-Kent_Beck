94063168b674fb8fd908d8402974d719a4da5876
Johannes W <Jeis2519@gmail.com>
2021-02-11 09:58:13 +0100

    add method rate

--- PART-I/src/main/java/tdd/money/Bank.java
+++ PART-I/src/main/java/tdd/money/Bank.java
@@ -5,4 +5,10 @@ public class Bank {
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
+
+    int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD"))
+                ? 2
+                : 1;
+    }
 }
