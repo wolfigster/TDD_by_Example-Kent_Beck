c8105b2055f38e75e2042e29e7721b2d9d2e735e
Johannes W <Jeis2519@gmail.com>
2021-02-11 10:58:30 +0100

    add test to check for addition with the same currency returns Money object

--- PART-I/src/test/java/tdd/money/MoneyTest.java
+++ PART-I/src/test/java/tdd/money/MoneyTest.java
@@ -90,4 +90,9 @@ public class MoneyTest extends TestCase {
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(20), result);
     }
+
+    public void testPlusSameCurrencyReturnsMoney() {
+        Expression sum = Money.dollar(1).plus(Money.dollar(1));
+        assertTrue(sum instanceof Money);
+    }
 }
