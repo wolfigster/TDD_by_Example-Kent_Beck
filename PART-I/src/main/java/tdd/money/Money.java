26d5770cf2ff3d06cdb8c4db2e93d43d6a0521a5
Johannes W <Jeis2519@gmail.com>
2021-02-11 08:52:30 +0100

    let Money.plus() return Sum

--- PART-I/src/main/java/tdd/money/Money.java
+++ PART-I/src/main/java/tdd/money/Money.java
@@ -22,7 +22,7 @@ public class Money implements Expression {
     }
 
     Expression plus(Money addend) {
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
     }
 
     String currency() {
