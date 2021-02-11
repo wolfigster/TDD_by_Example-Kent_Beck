8da4de66d7b208427a7cedacc8f3b5f632d70288
Johannes W <Jeis2519@gmail.com>
2021-02-11 09:14:31 +0100

    implement Bank.reduce()

--- PART-I/src/main/java/tdd/money/Bank.java
+++ PART-I/src/main/java/tdd/money/Bank.java
@@ -3,6 +3,8 @@ package tdd.money;
 public class Bank {
 
     Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        Sum sum = (Sum) source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, to);
     }
 }
