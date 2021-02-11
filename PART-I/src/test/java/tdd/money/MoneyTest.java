633351fe2cd9f78f0ff3671902ad16ff1b1b972e
Johannes W <Jeis2519@gmail.com>
2021-02-11 08:32:00 +0100

    add a reduced Expression for applying exchange rates

--- PART-I/src/test/java/tdd/money/MoneyTest.java
+++ PART-I/src/test/java/tdd/money/MoneyTest.java
@@ -25,5 +25,7 @@ public class MoneyTest extends TestCase {
     public void testSimpleAddition() {
         Money sum = Money.dollar(5).plus(Money.dollar(5));
         assertEquals(Money.dollar(10), sum);
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 }
